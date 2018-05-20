58030f01a2222533a5a29fe815592a7a474be7ee
RJP-Ricky <39267468+RJP-Ricky@users.noreply.github.com>
2018-05-20 11:19:57 +0100

    Update Assets Folder

---
+++ Assets/LoadMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.name == "FinalBoxFloor")
+            SceneManager.LoadScene(2);
+    }
+}
